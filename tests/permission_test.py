516785bba7cc345d47876a36683bf3b8ea6824af
Ryan Wehrle <ryan.wehrle@ansys.com>
2024-06-05 15:13:09 -0400

    Fix style issues

--- tests/permission_test.py
+++ tests/permission_test.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import time
 
-from ansys.hps.dt_client.data_transfer import AsyncClient, AsyncDataTransferApi, Client, DataTransferApi
+from ansys.hps.dt_client.data_transfer import Client, DataTransferApi
 from ansys.hps.dt_client.data_transfer.models.ops import OperationState
 from ansys.hps.dt_client.data_transfer.models.permissions import Resource, RoleQuery
 
@@ -13,7 +13,7 @@ def test_permission(binary_path, access_token):
         external_url="http://localhost:1091",
         run_client_binary=True,
         binary_path=binary_path,
-        token=access_token
+        token=access_token,
     ) as api_client:
         api_instance = DataTransferApi(api_client)
         with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
@@ -27,18 +27,4 @@ def test_permission(binary_path, access_token):
             if resp[0].state == OperationState.Succeeded:
                 break
         resp = api_instance.get_permissions([RoleQuery(resource=Resource(path=os.path.basename(temp_file.name)))])
-        assert len(resp.permissions) > 0
-
-
-# async def test_permission(binary_path):
-#     with AsyncClient(
-#         data_transfer_url="https://localhost:8443/hps/dts/api/v1",
-#         external_url="http://localhost:1091",
-#         run_client_binary=True,
-#         binary_path=binary_path,
-#     ) as api_client:
-#         api_instance = AsyncDataTransferApi(api_client)
-#         with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
-#             temp_file.write("Mock file")
-#         resp = await api_instance.upload_file("any", os.path.basename(temp_file.name), temp_file.name)
-#         assert resp.id is not None
+        assert len(resp.permissions) == 0
