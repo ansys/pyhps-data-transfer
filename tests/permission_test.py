50692fd8e494758ecb019778ac6b76a468e592cb
Michal Pawlik <michal.pawlik@ansys.com>
2024-06-07 14:05:17 +0200

    Working

--- tests/permission_test.py
+++ tests/permission_test.py
@@ -4,7 +4,7 @@ import time
 
 from ansys.hps.dt_client.data_transfer import DataTransferApi
 from ansys.hps.dt_client.data_transfer.models.ops import OperationState
-from ansys.hps.dt_client.data_transfer.models.permissions import Resource, RoleQuery
+from ansys.hps.dt_client.data_transfer.models.permissions import Resource, RoleAssignment, Subject
 
 
 def test_permissions(client, user_id):
@@ -15,11 +15,20 @@ def test_permissions(client, user_id):
     operation_id = resp.id
     assert operation_id is not None
     for _ in range(10):
-        time.sleep(1)
-        resp = api_instance.operations([operation_id])
-        if resp[0].state == OperationState.Succeeded:
+        time.sleep(0.5)
+        op = api_instance.operations([operation_id])[0]
+        if op.state in [OperationState.Succeeded, OperationState.Failed]:
             break
+    assert op.state == OperationState.Succeeded
 
-    resp = api_instance.get_permissions([RoleQuery(resource=Resource(path=os.path.basename(temp_file.name)))])
+    api_instance.set_permissions(
+        [
+            RoleAssignment(
+                resource=Resource(path=os.path.basename(temp_file.name), type="document"),
+                role="reader",
+                subject=Subject(id=user_id, type="user"),
+            )
+        ]
+    )
 
-    assert len(resp.permissions) == 0
+    # assert len(resp.permissions) == 0
