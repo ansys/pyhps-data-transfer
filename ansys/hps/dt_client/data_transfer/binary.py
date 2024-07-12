9970c70dd58f4c3167f984b13df06f40c638ea67
Michal Pawlik <michal.pawlik@ansys.com>
2024-07-12 14:57:18 +0200

    Only start the binary on start call

--- ansys/hps/dt_client/data_transfer/binary.py
+++ ansys/hps/dt_client/data_transfer/binary.py
@@ -5,6 +5,7 @@ import socket
 import stat
 import subprocess
 import threading
+import time
 
 log = logging.getLogger(__name__)
 
@@ -12,91 +13,117 @@ log = logging.getLogger(__name__)
 class Binary:
     def __init__(
         self,
+        # Required
         binary_path: str,
         data_transfer_url: str,
+        # Process related settings
+        log: bool = True,
+        monitor_interval: float = 0.5,
+        # Worker config settings
         external_url: str = None,
         token: str = None,
         port: int = None,
-        log: bool = True,
+        verbosity: int = 1,
+        insecure: bool = False,
+        debug: bool = False,
+        docs: bool = False,
     ):
-        # Retrieve an open port
-        if port is None:
-            port = self._get_open_port()
+        # Process related settings
+        self._base_args = []
+        self._args = []
+        self._binary_path = binary_path
+        self._capture_log = log
+        self._monitor_interval = monitor_interval
+        self._stop = threading.Event()
 
-        if not binary_path or not os.path.exists(binary_path):
+        # Worker config settings
+        self._debug = debug
+        self._data_transfer_url = data_transfer_url
+        self._verbosity = verbosity
+        self._port = port
+        self._token = token
+        self._external_url = external_url
+        self._insecure = insecure
+        self._docs = docs
+
+    def start(self):
+        if self._process is not None and self._process.returncode is None:
+            raise RuntimeError("Worker already started.")
+
+        self._stop.clear()
+
+        # Retrieve an open port
+        if self._port is None:
+            self._port = self._get_open_port()
+
+        if not self._binary_path or not os.path.exists(self._binary_path):
             # TODO - retrieve the binary?
             raise os.error("Binary not found.")
 
         # Mark binary as executable
-        if not os.access(binary_path, os.X_OK):
-            st = os.stat(binary_path)
-            os.chmod(binary_path, st.st_mode | stat.S_IEXEC)
+        if not os.access(self._binary_path, os.X_OK):
+            st = os.stat(self._binary_path)
+            os.chmod(self._binary_path, st.st_mode | stat.S_IEXEC)
 
-        # Rather pass token via env
-        self.args = [
-            binary_path,
-            "--dt-url",
-            data_transfer_url,
-            "--docs",
-            "--insecure",
-            "-v",
-            "2",
-            # "-d",
-            "--port",
-            str(port),
+        self._base_args = [
+            self._binary_path,
             "--log-types",
             "console",
         ]
 
-        # TODO: use shell=True on subprocess calls
-        if external_url is None:
-            args = " ".join(self.args)
+        if self._external_url is None:
+            args = " ".join(self._base_args)
             resp = subprocess.run(f"{args} config show", capture_output=True, text=True, shell=True)
             config = json.loads(resp.stdout)
-            external_url = config.get("external_url", None)
+            self._external_url = config.get("external_url", None)
 
-        self.external_url = external_url
+        self._build_args()
 
-        self.args.extend(
-            [
-                "--external-url",
-                external_url,
-            ]
-        )
+        self._process = None
 
-        if token is not None:
-            self.args.extend(
-                [
-                    "-t",
-                    f"Bearer {token}",
-                ]
-            )
+        if self._debug:
+            log.info(f"Starting worker: {args}")
 
-        self.process = None
-
-    def start(self):
-        args = " ".join(self.args)
         # if False:
-        proc = self.process = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        t = threading.Thread(target=self._monitor, args=())
+        t.daemon = True
+        t.start()
 
-        if log:
-            t = threading.Thread(target=self._log_output, args=(proc,))
+        if self._capture_log:
+            t = threading.Thread(target=self._log_output, args=())
             t.daemon = True
             t.start()
 
     def stop(self):
+        if self._process is None:
+            return
+        self._stop.set()
+        # TODO use /shutdown endpoint
         self.process.kill()
 
     def args_str(self):
         return " ".join(self.args)
 
-    def _log_output(self, proc):
-        while proc.poll() is None:
-            line = proc.stdout.readline()
+    def _log_output(self):
+        while not self._stop.is_set():
+            line = self._process.stdout.readline()
             if not line:
                 break
             line = line.decode().strip()
-            log.info("Binary: %s" % line)
+            log.info("Worker: %s" % line)
+
+    def _monitor(self):
+        while not self._stop.is_set():
+            if self._process is None:
+                args = " ".join(self._args)
+                self._process = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            ret_code = self._process.poll()
+            if ret_code is not None:
+                log.warn(f"Worker exited with code {ret_code}, restarting ...")
+                self._process = None
+
+            time.sleep(self._monitor_interval)
 
     def _get_open_port(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -105,3 +132,48 @@ class Binary:
         port = s.getsockname()[1]
         s.close()
         return port
+
+    def _build_args(self):
+        self.args = list(self._base_args)
+        self.args.extend(
+            [
+                "--dt-url",
+                self._data_transfer_url,
+                "--port",
+                str(self._port),
+                "--log-types",
+                "console",
+            ]
+        )
+
+        self.args.extend(
+            [
+                "-v",
+                str(self._verbosity),
+            ]
+        )
+
+        if self._docs:
+            self.args.append("--docs")
+
+        if self._insecure:
+            self.args.append("--insecure")
+
+        if self._debug:
+            self.args.append("--debug")
+
+        if self._external_url is not None:
+            self.args.extend(
+                [
+                    "--external-url",
+                    self._external_url,
+                ]
+            )
+
+        if self._token is not None:
+            self.args.extend(
+                [
+                    "-t",
+                    f"Bearer {self._token}",
+                ]
+            )
