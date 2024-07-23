9818bf26d90b462c316de3894d1fd1cf81c7e500
saimanikant <saimanikant@gmail.com>
2024-07-23 10:29:15 +0100

    add extention

--- ansys/hps/data_transfer/client/client.py
+++ ansys/hps/data_transfer/client/client.py
@@ -115,7 +115,7 @@ class ClientBase:
 
         platform_str = self._platform()
         log.debug(f"Downloading binary for platform '{platform_str}' from {dt_url} to {bin_path}")
-        url = f"/binaries/worker/{platform_str}/hpsdata"
+        url = f"/binaries/worker/{platform_str}/hpsdata{bin_ext}"
         try:
             with open(bin_path, "wb") as f, session.stream("GET", url) as resp:
                 resp.read()
