632cef4b393723612099ab11749aee7425cb6e3d
Michal Pawlik <michal.pawlik@ansys.com>
2024-07-26 15:47:50 +0200

    Working

--- ansys/hps/data_transfer/client/binary.py
+++ ansys/hps/data_transfer/client/binary.py
@@ -87,8 +87,8 @@ class Binary:
 
         self._base_args = []
         self._args = []
-        self._stop = threading.Event()
-        self._prepared = threading.Event()
+        self._stop = None
+        self._prepared = None
         self._process = None
 
     def __getstate__(self):
@@ -106,7 +106,8 @@ class Binary:
         if self._process is not None and self._process.returncode is None:
             raise BinaryError("Worker already started.")
 
-        self._stop.clear()
+        self._stop = threading.Event()
+        self._prepared = threading.Event()
 
         bin_path = self._config.path
         if not bin_path or not os.path.exists(bin_path):
@@ -135,6 +136,7 @@ class Binary:
     def stop(self, wait=5.0):
         if self._process is None:
             return
+
         self._stop.set()
         self._prepared.clear()
 
