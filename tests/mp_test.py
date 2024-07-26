12a8e010ce8d726a22d39483de91d837cef1551a
Michal Pawlik <michal.pawlik@ansys.com>
2024-07-26 16:23:44 +0200

    Clean up multiprocessing test

--- tests/mp_test.py
+++ tests/mp_test.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import logging
 import multiprocessing as mp
 
-from ansys.hps.data_transfer.client import DataTransferApi
+from ansys.hps.data_transfer.client import AsyncDataTransferApi, DataTransferApi
 
 log = logging.getLogger(__name__)
 
@@ -11,7 +11,7 @@ def _check_storage(dt_api):
     log.info(dt_api.storages())
 
 
-def test_mp_support(storage_path, client):
+def test_mp_support(client):
     api = DataTransferApi(client)
     api.status(wait=True)
 
@@ -21,5 +21,17 @@ def test_mp_support(storage_path, client):
 
     pool = concurrent.futures.ProcessPoolExecutor()
     f = pool.submit(_check_storage, api)
+    concurrent.futures.wait([f])
 
+
+async def test_async_mp_support(async_client):
+    api = AsyncDataTransferApi(async_client)
+    await api.status(wait=True)
+
+    p = mp.Process(target=_check_storage, args=(api,))
+    p.start()
+    p.join()
+
+    pool = concurrent.futures.ProcessPoolExecutor()
+    f = pool.submit(_check_storage, api)
     concurrent.futures.wait([f])
