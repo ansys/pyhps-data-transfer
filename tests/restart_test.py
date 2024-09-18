72016cff73e9d10a5cb430d9194dca6bef9a9b2b
Michal Pawlik <michal.pawlik@ansys.com>
2024-09-18 15:55:22 +0200

    Add tests for binary death and restart

---
+++ tests/restart_test.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+import time
+
+import psutil
+
+from ansys.hps.data_transfer.client import AsyncDataTransferApi, DataTransferApi
+
+log = logging.getLogger(__name__)
+
+
+def test_restart(client):
+    # Let it start up
+    api = DataTransferApi(client)
+    api.status(wait=True)
+
+    time.sleep(0.2)
+
+    # Kill the binary and wait for it to restart
+    p = psutil.Process(client.binary._process.pid)
+    p.kill()
+    time.sleep(0.2)
+    # assert client.binary._process.poll() is not None
+
+    api.status(wait=True, timeout=20)
+
+
+async def test_async_restart(client):
+    # Let it start up
+    api = AsyncDataTransferApi(client)
+    await api.status(wait=True)
+
+    await asyncio.sleep(0.2)
+
+    # Kill the binary and wait for it to restart
+    p = psutil.Process(client.binary._process.pid)
+    p.kill()
+    await asyncio.sleep(0.2)
+    # assert client.binary._process.poll() is not None
+
+    await api.status(wait=True, timeout=20)
