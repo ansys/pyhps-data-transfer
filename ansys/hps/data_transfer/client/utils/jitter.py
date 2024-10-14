62a107949e7ec58f302ba8c8b48680c204bac8d9
Michal Pawlik <michal.pawlik@ansys.com>
2024-10-14 16:47:18 +0200

    Add attempts cap parameter

--- ansys/hps/data_transfer/client/utils/jitter.py
+++ ansys/hps/data_transfer/client/utils/jitter.py
@@ -1,7 +1,9 @@
 from random import uniform
 
 
-def get_expo_backoff(base: float, attempts: int = 1, cap: float = 100_000_000, jitter: bool = True):
+def get_expo_backoff(
+    base: float, attempts: int = 1, cap: float = 100_000_000, attempts_cap: int = 100_000_000, jitter: bool = True
+):
     """
     Returns a backoff value https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
     :param base: The time to sleep in the first attempt.
@@ -11,6 +13,7 @@ def get_expo_backoff(base: float, attempts: int = 1, cap: float = 100_000_000, j
     """
     # Full jitter formula
     # https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+    attempts = min(attempts, attempts_cap)
     if jitter:
         try:
             return uniform(base, min(cap, base * 2 ** (attempts - 1)))
