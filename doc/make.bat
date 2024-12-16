10b6809128e20b46b1645bbc974e53ef5d8950a8
saimanikant <saimanikant@gmail.com>
2024-12-16 15:17:43 +0000

    docs build latex

--- doc/make.bat
+++ doc/make.bat
@@ -29,6 +29,11 @@ if errorlevel 9009 (
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
+:pdf
+	%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+	cd "%BUILDDIR%\latex"
+	pdflatex \*.tex --interaction=nonstopmode
+
 :clean
 rmdir /s /q %BUILDDIR% > /NUL 2>&1
 for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
