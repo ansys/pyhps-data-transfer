76776eb89dbd50ef6c929b51a6339e01b4cbd1a7
sashankh01 <sashankh01@users.noreply.github.com>
2025-01-09 17:28:23 +0000

    Version upgrade to 0.1.2

--- ansys/hps/data_transfer/client/__version__.py
+++ ansys/hps/data_transfer/client/__version__.py
@@ -6,7 +6,7 @@
 # Author(s): M.Pawlik
 # ----------------------------------------------------------
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __internal_version__ = "0.1.1"
 __company__ = "ANSYS Inc"
 __company_short__ = "Ansys"
