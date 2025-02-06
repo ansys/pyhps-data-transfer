3a9a2d23049792f25134cda74fb0aba45d98a771
Raja Pise <raja.pise@ansys.com>
2025-02-06 12:16:08 +0530

    Formated code

--- ansys/hps/data_transfer/client/utils/otel_config.py
+++ ansys/hps/data_transfer/client/utils/otel_config.py
@@ -1,12 +1,13 @@
 import os
 
-def set_otel_config(exporter_url, resource_attributes=None,headers=None, exporter_type=None):
-    """ Set data transfer worker Otel configuration using environment variables before starting data transfer worker
-        ANSYS_DT_OTEL__EXPORTER_URL - Otel exporter url 
-        ANSYS_DT_OTEL__RESOURCE_ATTRIBUTES - key-value pairs of resource attributes to be passed to the Otel SDK
-        ANSYS_DT_OTEL__HEADERS - key-value pairs of headers to be associated with gRPC requests
-        ANSYS_DT_OTEL__EXPORTER_TYPE - Otel exporter type
-        ANSYS_DT_OTEL__ENABLED - enables Otel 
+
+def set_otel_config(exporter_url, resource_attributes=None, headers=None, exporter_type=None):
+    """Set data transfer worker Otel configuration using environment variables before starting data transfer worker
+    ANSYS_DT_OTEL__EXPORTER_URL - Otel exporter url
+    ANSYS_DT_OTEL__RESOURCE_ATTRIBUTES - key-value pairs of resource attributes to be passed to the Otel SDK
+    ANSYS_DT_OTEL__HEADERS - key-value pairs of headers to be associated with gRPC requests
+    ANSYS_DT_OTEL__EXPORTER_TYPE - Otel exporter type
+    ANSYS_DT_OTEL__ENABLED - enables Otel
     """
     os.environ["ANSYS_DT_OTEL__ENABLED"] = "True"
     if exporter_type != None:
@@ -17,4 +18,3 @@ def set_otel_config(exporter_url, resource_attributes=None,headers=None, exporte
         os.environ["ANSYS_DT_OTEL__RESOURCE_ATTRIBUTES"] = str(resource_attributes)
     if headers != None:
         os.environ["ANSYS_DT_OTEL__HEADERS"] = str(headers)
-    
